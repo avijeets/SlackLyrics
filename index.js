b96a686c7acc0f99bf18299facf044fb2d349c81
avijeet <a@avijeets.com>
2016-06-09 02:13:58 -0400

    json + running node

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var url = require('url');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (3000 || process.env.PORT));
+app.get('/', function(req, res){ res.send('Server running.'); });
